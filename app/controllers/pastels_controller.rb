f85c09db589966d86c5cffc6262a0078621e060f
Sharon Moriel <shmoriel@gmail.com>
2019-11-19 13:16:32 +0000

    changes to the pastel and order controllers

--- app/controllers/pastels_controller.rb
+++ app/controllers/pastels_controller.rb
@@ -6,17 +6,15 @@ class PastelsController < ApplicationController
   end
 
   def new
-    @user = User.find(params[:user_id])
     @pastel = Pastel.new
   end
 
   def create
-    @user = User.find(params[:user_id])
     @pastel = Pastel.new(pastel_params)
-    @pastel.user = @user
+    @pastel.user = current_user
 
     if @pastel.save
-      redirect_to user_pastels_path(@user)
+      redirect_to user_path(current_user)
     else
       render :new
     end
@@ -42,6 +40,6 @@ class PastelsController < ApplicationController
   end
 
   def pastel_params
-    params.require(:pastel).permit(:name, :description, :stock, :photo)
+    params.require(:pastel).permit(:name, :description, :price, :stock, :photo)
   end
 end
