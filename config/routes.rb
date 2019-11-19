d83fcecea1bf5c3958f1e90a9b20ae583e314a3d
Sharon Moriel <shmoriel@gmail.com>
2019-11-19 15:49:22 +0000

    added two menu items to the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  # root to: 'pages#home'
+  root to: 'pastels#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show]
 
