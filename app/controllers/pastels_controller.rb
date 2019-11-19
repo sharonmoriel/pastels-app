486982e9b88c963526dc08fbf10e491f86ff4004
Alexandre Araujo <alelaraujo@gmail.com>
2019-11-19 10:58:39 +0000

    created the pastels controller

--- app/controllers/pastels_controller.rb
+++ app/controllers/pastels_controller.rb
@@ -1,11 +1,23 @@
 class PastelsController < ApplicationController
+  before_action :set_pastel, only: [:show, :edit, :update, :destroy]
+
   def index
+    @pastels = Pastel.all
   end
 
   def new
+    @pastel = Pastel.new
   end
 
   def create
+    @pastel = Pastel.new(garden_params)
+    @pastel.user = current_user
+
+    if @pastel.save
+      redirect_to pastel_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -18,5 +30,16 @@ class PastelsController < ApplicationController
   end
 
   def destroy
+    @pastel.destroy
+  end
+
+  private
+
+  def set_pastel
+    @pastel = Pastel.find(params[:id])
+  end
+
+  def pastel_params
+    params.require(:pastel).permit(:name, :size, :photo_url, :covered, :location)
   end
 end
