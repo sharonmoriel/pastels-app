1fe0c93165ad5290b5be9c099b7343c4ef56c544
Sharon Moriel <shmoriel@gmail.com>
2019-11-20 13:17:10 +0000

    added client side validations gem, and sweet alert package

--- Gemfile
+++ Gemfile
@@ -18,6 +18,9 @@ gem 'uglifier'
 gem 'webpacker'
 gem 'cloudinary', '~> 1.9.1'
 gem 'pundit'
+gem 'client_side_validations'
+gem 'client_side_validations-simple_form'
+
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,7 @@
 import "bootstrap";
+import '@client-side-validations/client-side-validations';
+import '@client-side-validations/simple-form/dist/simple-form.bootstrap4';
+
 
 document.querySelector('.img__btn').addEventListener('click', function() {
   document.querySelector('.cont').classList.toggle('s--signup');
