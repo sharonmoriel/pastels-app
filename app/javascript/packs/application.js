c0a177d4562e01b14873179037565217f1c6038d
Sharon Moriel <shmoriel@gmail.com>
2019-11-21 15:05:56 +0000

    fix login/signup js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,8 @@ import '@client-side-validations/simple-form/dist/simple-form.bootstrap4';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/mapbox.js';
 
-// document.querySelector('.img__btn').addEventListener('click', function() {
-//   document.querySelector('.cont').classList.toggle('s--signup');
-// });
-
-
-
 initMapbox();
 
+document.querySelector('.img__btn').addEventListener('click', function() {
+  document.querySelector('.cont').classList.toggle('s--signup');
+});
