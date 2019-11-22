577491d595571d23f4d18eeecb82283979dcad33
Alexandre Araujo <alelaraujo@gmail.com>
2019-11-22 14:16:45 +0000

    created the orders to me

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
     authorize @user
     @my_pastels = Pastel.where(user_id: @user.id)
     @my_orders = Order.where(user_id: @user.id)
+    @orders_to_me = Order.where(pastel_id: @my_pastels)
+
 
   end
 
