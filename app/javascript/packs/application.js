bc2164e6e0a0bce44b2cad70a94d5d80defb4a73
Sharon Moriel <shmoriel@gmail.com>
2019-11-22 10:50:28 +0000

    fix js for map and login forms

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,20 +3,33 @@ import '@client-side-validations/client-side-validations';
 import '@client-side-validations/simple-form/dist/simple-form.bootstrap4';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/mapbox.js';
+// import { flatpickr } from "flatpickr";
 
 initMapbox();
 
-// document.querySelector('.img__btn').addEventListener('click', function() {
-//   document.querySelector('.cont').classList.toggle('s--signup');
 
-$('#query').focus(function()
-{
-  $(this).animate({ width: '+=100' }, 'slow');
-}).blur(function()
-{
-  $(this).animate({ width: '-=50' }, 'slow');
-});
+const imgBtn = document.querySelector('.img__btn')
 
-$(document).ready(function(){
-    flatpickr('#flatpickr-input');
-});
+if(imgBtn !== null) {
+  imgBtn.addEventListener("click", (event) => {
+    console.log(event.currentTarget);
+    document.querySelector('.cont').classList.toggle('s--signup');
+  });
+}
+
+const searchQuery = $('#query')
+
+if(searchQuery !== null) {
+  searchQuery.focus(function()
+  {
+    $(this).animate({ width: '+=100' }, 'slow');
+  }).blur(function()
+  {
+    $(this).animate({ width: '-=50' }, 'slow');
+  });
+}
+
+
+// $(document).ready(function(){
+//     flatpickr('#flatpickr-input');
+// });
